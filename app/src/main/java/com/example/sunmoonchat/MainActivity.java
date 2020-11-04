4ecc691973779e7ce1b69de3720a8bffc89680e0
tkddnr924 <tkddnr924@naver.com>
2020-11-05 00:51:54 +0900

    app full screen & remove ToolBar

--- app/src/main/java/com/example/sunmoonchat/MainActivity.java
+++ app/src/main/java/com/example/sunmoonchat/MainActivity.java
@@ -3,12 +3,18 @@ package com.example.sunmoonchat;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // 안드로이드 상태 바 제거 (전체화면으로 보여줌)
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         setContentView(R.layout.activity_main);
     }
 }
